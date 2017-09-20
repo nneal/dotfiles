cfd3d4c195e91e820349de36b2f84a9a5ca2b2a8
Neal Kemp <nneal@users.noreply.github.com>
2017-09-20 11:07:16 -0500

    Set up rbenv shim path and silence asdf

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -1,6 +1,8 @@
 # ensure dotfiles bin directory is loaded first
 PATH="$HOME/.bin:/usr/local/sbin:$PATH"
 
+export SILENCE_RBENV_DEPRECATION=1
+
 # load ASDF, falling back to rbenv if not available
 if [ -d "$HOME/.asdf" ]; then
   . $HOME/.asdf/asdf.sh
